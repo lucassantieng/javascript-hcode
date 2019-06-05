8323c10b987380aa5ff75c89765ae89a8e34e066
Santi L <lucassantieng@gmail.com>
2019-06-05 16:27:14 -0300

    Commit aula inicial calculadora

--- projetos/calculadora/scripts/controller/CalcController.js
+++ projetos/calculadora/scripts/controller/CalcController.js
@@ -4,6 +4,19 @@ class CalcController {
 
         this._displayCalc = "0";
         this._currentDate;
+        this.initialize(); //toda vez que inicializa a calculadora eu vou executar esse método e selecionar objeto display, hora e data.
+    }
+
+    initialize(){
+            let displayCalcEl = document.querySelector("#display"); //aqui estou selecionando meu objeto html e armazenando em uma variavel para poder realizar a manipulção nesse elemento
+            let dateEl = document.querySelector("#data");
+            let timeEl = document.querySelector("#hora");
+
+            displayCalcEl.innerHTML = "2706"; //aqui estou inserindo um valor exatamento no objeto selecionado
+            dateEl.innerHTML = "05/06/2019";
+            timeEl.innerHTML = "19:00";
+
+            
     }
 
     get displayCalc(){
@@ -21,5 +34,7 @@ class CalcController {
     set dataAtual(valor){
         this._currentDate;
     }
+
+
     
 }
