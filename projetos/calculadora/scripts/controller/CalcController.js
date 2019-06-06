da792cadd9265ca11cbd79f10a6dca00368270e1
Santi L <lucassantieng@gmail.com>
2019-06-06 17:48:51 -0300

    Commit retatoração do código 1

--- projetos/calculadora/scripts/controller/CalcController.js
+++ projetos/calculadora/scripts/controller/CalcController.js
@@ -2,15 +2,19 @@ class CalcController {
 
     constructor(){
 
+
+        this._displayCalcEl = document.querySelector()
         this._displayCalc = "0";
         this._currentDate;
         this.initialize(); //toda vez que inicializa a calculadora eu vou executar esse método e selecionar objeto display, hora e data.
     }
 
     initialize(){
-            let displayCalcEl = document.querySelector("#display"); //aqui estou selecionando meu objeto html e armazenando em uma variavel para poder realizar a manipulção nesse elemento
+            /*let displayCalcEl = document.querySelector("#display"); //aqui estou selecionando meu objeto html e armazenando em uma variavel para poder realizar a manipulção nesse elemento
             let dateEl = document.querySelector("#data");
             let timeEl = document.querySelector("#hora");
+
+            */  //todo esse código foi refatorado.
 
             displayCalcEl.innerHTML = "2706"; //aqui estou inserindo um valor exatamento no objeto selecionado
             dateEl.innerHTML = "05/06/2019";
